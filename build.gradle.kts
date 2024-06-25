8e1625638cd32ff3a2ded13bfc148583f2cfc34e
Dltmd202 <dltmd202@gmail.com>
2024-06-25 16:36:44 +0900

    chore: 테스트용 h2 세팅
    Co-authored-by: GGHDMS <gnivy303@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,5 +48,6 @@ subprojects {
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+        testRuntimeOnly("com.h2database:h2")
     }
 }
