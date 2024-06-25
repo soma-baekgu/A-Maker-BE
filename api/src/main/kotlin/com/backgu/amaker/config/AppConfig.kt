e5043a220bbf270ca3cba55925cb22f21ec3555f
Dltmd202 <dltmd202@gmail.com>
2024-06-26 02:22:52 +0900

    feat: RestClient를 예외 래퍼 AOP 개발

---
+++ api/src/main/kotlin/com/backgu/amaker/config/AppConfig.kt
@@ -0,0 +1,8 @@
+package com.backgu.amaker.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.EnableAspectJAutoProxy
+
+@Configuration
+@EnableAspectJAutoProxy
+class AppConfig
