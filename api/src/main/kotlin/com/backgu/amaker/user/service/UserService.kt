24b053b07ebfce3c3af6b79d16e8f1b71f0a33e5
Dltmd202 <dltmd202@gmail.com>
2024-06-27 08:32:30 +0900

    feat: 유저 생성 또는 조회 로직 개발

--- api/src/main/kotlin/com/backgu/amaker/user/service/UserService.kt
+++ api/src/main/kotlin/com/backgu/amaker/user/service/UserService.kt
@@ -1,11 +1,10 @@
 package com.backgu.amaker.user.service
 
-import com.backgu.amaker.user.domain.User
-import com.backgu.amaker.user.dto.UserRequest
+import com.backgu.amaker.user.dto.UserCreateDto
+import com.backgu.amaker.user.dto.UserDto
 import com.backgu.amaker.user.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.UUID
 
 @Service
 @Transactional(readOnly = true)
@@ -13,13 +12,15 @@ class UserService(
     val userRepository: UserRepository,
 ) {
     @Transactional
-    fun saveUser(request: UserRequest): UUID =
-        userRepository
-            .save(
-                User(
-                    name = request.name,
-                    email = request.email,
-                    picture = request.picture,
-                ),
-            ).id
+    fun saveUser(create: UserCreateDto): UserDto =
+        UserDto.of(
+            userRepository.save(create.toEntity()),
+        )
+
+    fun saveOrGetUser(user: UserCreateDto): UserDto =
+        userRepository.findByEmail(user.email)?.let { UserDto.of(it) }
+            ?: saveUser(user)
+
+    fun getByEmail(email: String): UserDto =
+        UserDto.of(userRepository.findByEmail(email) ?: throw IllegalArgumentException("User not found"))
 }
