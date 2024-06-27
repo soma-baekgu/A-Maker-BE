5f2c51e9539a2ffcf3eb7e0047e2eca515e1614d
Dltmd202 <dltmd202@gmail.com>
2024-06-27 10:27:54 +0900

    refactor: AuthController 리팩토링

--- api/src/main/kotlin/com/backgu/amaker/auth/controller/AuthController.kt
+++ api/src/main/kotlin/com/backgu/amaker/auth/controller/AuthController.kt
@@ -1,7 +1,6 @@
 package com.backgu.amaker.auth.controller
 
 import com.backgu.amaker.auth.config.AuthConfig
-import com.backgu.amaker.auth.service.AuthService
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/auth")
 class AuthController(
     val authConfig: AuthConfig,
-    val authService: AuthService,
 ) {
     @GetMapping("/oauth/google")
     fun googleAuth(response: HttpServletResponse) {
