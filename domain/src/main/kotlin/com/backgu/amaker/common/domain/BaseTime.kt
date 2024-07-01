4be8b3e2840515ae2cc8035de79e3410826a95d0
Dltmd202 <dltmd202@gmail.com>
2024-07-01 13:31:52 +0900

    fix: BaseTime을 abstract class로 수정

--- domain/src/main/kotlin/com/backgu/amaker/common/domain/BaseTime.kt
+++ domain/src/main/kotlin/com/backgu/amaker/common/domain/BaseTime.kt
@@ -2,7 +2,7 @@ package com.backgu.amaker.common.domain
 
 import java.time.LocalDateTime
 
-open class BaseTime(
+abstract class BaseTime(
     val createdAt: LocalDateTime = LocalDateTime.now(),
     val updatedAt: LocalDateTime = LocalDateTime.now(),
 )
