799cad86642527c37f04d359008f651682b22031
Dltmd202 <dltmd202@gmail.com>
2024-07-02 23:03:00 +0900

    refactor: conflict 해결

--- api/src/main/kotlin/com/backgu/amaker/auth/controller/AuthApiController.kt
+++ api/src/main/kotlin/com/backgu/amaker/auth/controller/AuthApiController.kt
@@ -4,6 +4,8 @@ import com.backgu.amaker.auth.config.AuthConfig
 import com.backgu.amaker.auth.dto.response.JwtTokenResponse
 import com.backgu.amaker.auth.dto.response.OAuthUrlResponse
 import com.backgu.amaker.auth.service.AuthFacadeService
+import com.backgu.amaker.common.dto.response.ApiResult
+import com.backgu.amaker.common.infra.ApiHandler
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,11 +17,14 @@ import org.springframework.web.bind.annotation.RestController
 class AuthApiController(
     val authConfig: AuthConfig,
     val authFacadeService: AuthFacadeService,
+    val apiHandler: ApiHandler,
 ) : AuthApiSwagger {
     @GetMapping("/oauth/google")
-    override fun googleAuth(): ResponseEntity<OAuthUrlResponse> =
+    override fun googleAuth(): ResponseEntity<ApiResult<OAuthUrlResponse>> =
         ResponseEntity.ok().body(
-            OAuthUrlResponse(authConfig.oauthUrl),
+            apiHandler.onSuccess(
+                OAuthUrlResponse(authConfig.oauthUrl),
+            ),
         )
 
     @GetMapping("/code/google")
@@ -28,8 +33,10 @@ class AuthApiController(
         @RequestParam(name = "scope", required = false) scope: String,
         @RequestParam(name = "authuser", required = false) authUser: String,
         @RequestParam(name = "prompt", required = false) prompt: String,
-    ): ResponseEntity<JwtTokenResponse> =
+    ): ResponseEntity<ApiResult<JwtTokenResponse>> =
         ResponseEntity.ok().body(
-            JwtTokenResponse.of(authFacadeService.googleLogin(authorizationCode)),
+            apiHandler.onSuccess(
+                JwtTokenResponse.of(authFacadeService.googleLogin(authorizationCode)),
+            ),
         )
 }
--- api/src/main/kotlin/com/backgu/amaker/workspace/controller/WorkspaceSwagger.kt
+++ api/src/main/kotlin/com/backgu/amaker/workspace/controller/WorkspaceSwagger.kt
@@ -1,6 +1,7 @@
 package com.backgu.amaker.workspace.controller
 
 import com.backgu.amaker.chat.dto.response.ChatRoomResponse
+import com.backgu.amaker.common.dto.response.ApiResult
 import com.backgu.amaker.security.JwtAuthentication
 import com.backgu.amaker.workspace.dto.request.WorkspaceCreateRequest
 import com.backgu.amaker.workspace.dto.response.WorkspaceResponse
@@ -8,13 +9,13 @@ import com.backgu.amaker.workspace.dto.response.WorkspacesResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.enums.ParameterIn
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 
 @Tag(name = "workspaces", description = "워크스페이스 API")
 interface WorkspaceSwagger {
@@ -24,13 +25,12 @@ interface WorkspaceSwagger {
             ApiResponse(
                 responseCode = "201",
                 description = "워크스페이스 생성 성공",
-                content = [Content(schema = Schema(implementation = Unit::class))],
             ),
         ],
     )
     fun createWorkspace(
         @Parameter(hidden = true) token: JwtAuthentication,
-        request: WorkspaceCreateRequest,
+        @RequestBody @Valid request: WorkspaceCreateRequest,
     ): ResponseEntity<Unit>
 
     @Operation(summary = "workspaces 조회", description = "유저가 참여하고 있는 모든 워크스페이스를 조회합니다.")
@@ -39,13 +39,12 @@ interface WorkspaceSwagger {
             ApiResponse(
                 responseCode = "200",
                 description = "워크스페이스 조회 성공",
-                content = [Content(schema = Schema(implementation = WorkspacesResponse::class))],
             ),
         ],
     )
     fun findWorkspaces(
         @Parameter(hidden = true) token: JwtAuthentication,
-    ): ResponseEntity<WorkspacesResponse>
+    ): ResponseEntity<ApiResult<WorkspacesResponse>>
 
     @Operation(summary = "기본 workspace 조회", description = "유저가 가장 최근에 참여한 워크스페이스를 조회합니다.")
     @ApiResponses(
@@ -53,13 +52,12 @@ interface WorkspaceSwagger {
             ApiResponse(
                 responseCode = "200",
                 description = "워크스페이스 조회 성공",
-                content = [Content(schema = Schema(implementation = WorkspaceResponse::class))],
             ),
         ],
     )
     fun getDefaultWorkspace(
         @Parameter(hidden = true) token: JwtAuthentication,
-    ): ResponseEntity<WorkspaceResponse>
+    ): ResponseEntity<ApiResult<WorkspaceResponse>>
 
     @Operation(summary = "그룹 채팅방 조회", description = "워크스페이스의 그룹 채팅방을 조회합니다.")
     @ApiResponses(
@@ -67,7 +65,6 @@ interface WorkspaceSwagger {
             ApiResponse(
                 responseCode = "200",
                 description = "그룹 채팅방 조회 성공",
-                content = [Content(schema = Schema(implementation = ChatRoomResponse::class))],
             ),
         ],
     )
@@ -76,5 +73,5 @@ interface WorkspaceSwagger {
         @Parameter(description = "워크스페이스 ID", required = true, `in` = ParameterIn.PATH)
         workspaceId: Long,
         @Parameter(hidden = true) token: JwtAuthentication,
-    ): ResponseEntity<ChatRoomResponse>
+    ): ResponseEntity<ApiResult<ChatRoomResponse>>
 }
