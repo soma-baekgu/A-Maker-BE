4c00f76d8e08f16c769585cbc1ecd38453a1e107
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-07-02 16:37:12 +0900

    feat: 그룹 채팅방 조회 기능 구현

--- api/src/main/kotlin/com/backgu/amaker/chat/repository/ChatRoomRepository.kt
+++ api/src/main/kotlin/com/backgu/amaker/chat/repository/ChatRoomRepository.kt
@@ -1,6 +1,12 @@
 package com.backgu.amaker.chat.repository
 
+import com.backgu.amaker.chat.domain.ChatRoomType
 import com.backgu.amaker.chat.jpa.ChatRoomEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ChatRoomRepository : JpaRepository<ChatRoomEntity, Long>
+interface ChatRoomRepository : JpaRepository<ChatRoomEntity, Long> {
+    fun findByWorkspaceIdAndChatRoomType(
+        workspaceId: Long,
+        chatRoomType: ChatRoomType,
+    ): ChatRoomEntity?
+}
--- api/src/main/kotlin/com/backgu/amaker/chat/service/ChatRoomService.kt
+++ api/src/main/kotlin/com/backgu/amaker/chat/service/ChatRoomService.kt
@@ -1,10 +1,16 @@
 package com.backgu.amaker.chat.service
 
 import com.backgu.amaker.chat.domain.ChatRoom
+import com.backgu.amaker.chat.domain.ChatRoomType
 import com.backgu.amaker.chat.jpa.ChatRoomEntity
 import com.backgu.amaker.chat.repository.ChatRoomRepository
+import com.backgu.amaker.workspace.domain.Workspace
+import io.github.oshai.kotlinlogging.KotlinLogging
+import jakarta.persistence.EntityNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+
+private val logger = KotlinLogging.logger {}
 
 @Service
 @Transactional(readOnly = true)
@@ -13,4 +19,15 @@ class ChatRoomService(
 ) {
     @Transactional
     fun save(chatRoom: ChatRoom): ChatRoom = chatRoomRepository.save(ChatRoomEntity.of(chatRoom)).toDomain()
+
+    fun getGroupChatRoomByWorkspace(
+        workspace: Workspace,
+        group: ChatRoomType,
+    ): ChatRoom =
+        chatRoomRepository.findByWorkspaceIdAndChatRoomType(workspace.id, ChatRoomType.GROUP)?.toDomain()
+            // TODO : 공통 에러처리 추후에 해줘야함
+            ?: run {
+                logger.error { "Group ChatRoom not found in Workspace ${workspace.id}" }
+                throw EntityNotFoundException("Group ChatRoom not found in Workspace ${workspace.id}")
+            }
 }
