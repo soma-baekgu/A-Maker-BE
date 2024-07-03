58c7dc56d27e9c91a9a5b350bbd315d1c6b153a9
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-07-03 16:04:59 +0900

    chore: 웹 소켓 설정 추가

---
+++ api/src/main/kotlin/com/backgu/amaker/chat/config/WebSocketConfig.kt
@@ -0,0 +1,26 @@
+package com.backgu.amaker.chat.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+    override fun configureMessageBroker(config: MessageBrokerRegistry) {
+        config.enableSimpleBroker("/sub")
+        config.setApplicationDestinationPrefixes("/pub")
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry
+            .addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+            .withSockJS()
+        registry
+            .addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+    }
+}
