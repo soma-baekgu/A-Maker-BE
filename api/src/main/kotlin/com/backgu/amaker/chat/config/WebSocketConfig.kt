6b33dbfca1580d6f226c4d8d9073ec69388c4b44
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-07-04 13:44:15 +0900

    chore: 웹 소켓 연결 요청시 jwt를 활용한 인증 Interceptor 기능 구현

--- api/src/main/kotlin/com/backgu/amaker/chat/config/WebSocketConfig.kt
+++ api/src/main/kotlin/com/backgu/amaker/chat/config/WebSocketConfig.kt
@@ -1,6 +1,8 @@
 package com.backgu.amaker.chat.config
 
+import com.backgu.amaker.chat.interceptor.JwtChannelInterceptor
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
@@ -8,7 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+class WebSocketConfig(
+    private val jwtChannelInterceptor: JwtChannelInterceptor,
+) : WebSocketMessageBrokerConfigurer {
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
         config.enableSimpleBroker("/sub")
         config.setApplicationDestinationPrefixes("/pub")
@@ -22,5 +26,9 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
         registry
             .addEndpoint("/ws")
             .setAllowedOriginPatterns("*")
+    }
+
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        registration.interceptors(jwtChannelInterceptor)
     }
 }
