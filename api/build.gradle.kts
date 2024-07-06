74f29a3cb6b0cd957fd1e750e302c68bab5e6dc5
Dltmd202 <dltmd202@gmail.com>
2024-07-06 19:04:53 +0900

    chore: javaMailSender와 thymleaf 의존성

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation("com.auth0:java-jwt:3.18.3")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
     // Swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
