6f3d814ff6e3bcbb421e6624a7d073b25720807b
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-07-07 12:52:55 +0900

    feat: 일반 채팅 채팅방에 pub 기능 구현

--- api/src/main/kotlin/com/backgu/amaker/chat/controller/ChatController.kt
+++ api/src/main/kotlin/com/backgu/amaker/chat/controller/ChatController.kt
@@ -1,10 +1,12 @@
 package com.backgu.amaker.chat.controller
 
 import com.backgu.amaker.chat.dto.request.GeneralChatCreateRequest
+import com.backgu.amaker.chat.dto.response.ChatResponse
 import com.backgu.amaker.chat.service.ChatFacadeService
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,10 +14,9 @@ class ChatController(
     private val chatFacadeService: ChatFacadeService,
 ) {
     @MessageMapping("/chat-rooms/{chat-rooms-id}/general")
+    @SendTo("/sub/chat-rooms/{chat-rooms-id}")
     fun sendGeneralChat(
-        @Payload chat: GeneralChatCreateRequest,
+        @Payload generalChatCreateRequest: GeneralChatCreateRequest,
         @DestinationVariable("chat-rooms-id") chatRoomId: Long,
-    ) {
-        chatFacadeService.createGeneralChat(chat.toDto(), chatRoomId)
-    }
+    ): ChatResponse = ChatResponse.of(chatFacadeService.createGeneralChat(generalChatCreateRequest.toDto(), chatRoomId))
 }
