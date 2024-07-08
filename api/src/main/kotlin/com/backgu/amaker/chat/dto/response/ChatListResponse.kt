9a68a16812e54dc6db82b35f44ba51eb817e2cf9
Dltmd202 <dltmd202@gmail.com>
2024-07-08 19:03:52 +0900

    feat: ChatResponse에 다음 커서 추가

--- api/src/main/kotlin/com/backgu/amaker/chat/dto/response/ChatListResponse.kt
+++ api/src/main/kotlin/com/backgu/amaker/chat/dto/response/ChatListResponse.kt
@@ -1,12 +1,18 @@
 package com.backgu.amaker.chat.dto.response
 
 import com.backgu.amaker.chat.dto.ChatListDto
+import io.swagger.v3.oas.annotations.media.Schema
 
 class ChatListResponse(
+    @Schema(description = "채팅방 ID", example = "1")
     val chatRoomId: Long,
+    @Schema(description = "채팅 커서", example = "101")
     val cursor: Long,
+    @Schema(description = "읽어올 채팅의 개수", example = "100", defaultValue = "20")
     val size: Int,
     val chatList: List<ChatResponse>,
+    @Schema(description = "다음 요청에 대한 커서", example = "100", defaultValue = "121")
+    val nextCursor: Long,
 ) {
     companion object {
         fun of(chatListDto: ChatListDto) =
@@ -15,6 +21,7 @@ class ChatListResponse(
                 cursor = chatListDto.cursor,
                 size = chatListDto.size,
                 chatList = chatListDto.chatList.map { ChatResponse.of(it) },
+                nextCursor = chatListDto.chatList.first().id,
             )
     }
 }
--- api/src/main/kotlin/com/backgu/amaker/chat/dto/response/ChatResponse.kt
+++ api/src/main/kotlin/com/backgu/amaker/chat/dto/response/ChatResponse.kt
@@ -2,15 +2,23 @@ package com.backgu.amaker.chat.dto.response
 
 import com.backgu.amaker.chat.domain.ChatType
 import com.backgu.amaker.chat.dto.ChatDto
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
 data class ChatResponse(
+    @Schema(description = "채팅 ID", example = "1")
     val id: Long,
+    @Schema(description = "유저 ID(현재 UUID)", example = "0000-0000-0000-0000000")
     val userId: String,
+    @Schema(description = "채팅방 ID", example = "1")
     val chatRoomId: Long,
+    @Schema(description = "채팅 내용", example = "안녕하세요")
     var content: String,
+    @Schema(description = "채팅 타입(GENERAL, REPLY, REACTION, TASK)", example = "GENERAL")
     val chatType: ChatType,
+    @Schema(description = "생성일시", example = "2021-05-29T00:00:00")
     val createdAt: LocalDateTime,
+    @Schema(description = "수정일시", example = "2021-05-29T00:00:00")
     val updatedAt: LocalDateTime,
 ) {
     companion object {
