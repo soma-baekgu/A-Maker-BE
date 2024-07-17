05ac6a29b182aed21b74d3607531ab259ba7925f
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-07-17 16:24:23 +0900

    fix: 잘못된 import 부분 수정

--- api/src/main/kotlin/com/backgu/amaker/common/exception/GlobalExceptionHandler.kt
+++ api/src/main/kotlin/com/backgu/amaker/common/exception/GlobalExceptionHandler.kt
@@ -3,34 +3,31 @@ package com.backgu.amaker.common.exception
 import com.backgu.amaker.common.dto.response.ApiError
 import com.backgu.amaker.common.infra.ApiHandler
 import io.github.oshai.kotlinlogging.KotlinLogging
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.context.support.DefaultMessageSourceResolvable
 import org.springframework.http.ResponseEntity
-import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException
-import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.method.annotation.HandlerMethodValidationException
 
 private val logger = KotlinLogging.logger {}
 
-@ControllerAdvice
+@RestControllerAdvice
 class GlobalExceptionHandler(
     private val apiHandler: ApiHandler,
 ) {
-    @ExceptionHandler(RuntimeException::class)
-    fun handleRuntimeException(e: RuntimeException): ResponseEntity<ApiError<Unit>> {
-        e.printStackTrace()
-        return ResponseEntity
-            .internalServerError()
-            .body(apiHandler.onFailure(StatusCode.INTERNAL_SERVER_ERROR))
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ApiError<Map<String, String>>> {
+    fun handleMethodArgumentNotValidException(
+        e: MethodArgumentNotValidException,
+        httpServletRequest: HttpServletRequest,
+    ): ResponseEntity<ApiError<Map<String, String>>> {
         val errorMap: Map<String, String>? =
             e.bindingResult?.fieldErrors?.associate { error ->
                 error.field to (error.defaultMessage ?: "Invalid value")
             }
 
+        logger.error { e.printStackTrace() }
         errorMap?.forEach { (field, message) ->
             logger.error { "Validation error on field: $field - Message: $message" }
         }
@@ -51,6 +48,7 @@ class GlobalExceptionHandler(
                 )
             }
 
+        logger.error { e.printStackTrace() }
         errorMap.forEach { (field, message) ->
             logger.error { "Validation error on field: $field - Message: $message" }
         }
@@ -63,4 +61,12 @@ class GlobalExceptionHandler(
     @ExceptionHandler(BusinessException::class)
     fun handleBusinessException(e: BusinessException): ResponseEntity<ApiError<Unit>> =
         ResponseEntity.status(e.httpStatus).body(apiHandler.onFailure(e.statusCode))
+
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(e: RuntimeException): ResponseEntity<ApiError<Unit>> {
+        e.printStackTrace()
+        return ResponseEntity
+            .internalServerError()
+            .body(apiHandler.onFailure(StatusCode.INTERNAL_SERVER_ERROR))
+    }
 }
