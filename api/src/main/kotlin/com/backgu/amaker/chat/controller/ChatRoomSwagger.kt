48e151f28b46341c1876218a562f82d2ad93b2e5
Dltmd202 <dltmd202@gmail.com>
2024-07-24 20:02:17 +0900

    chore: lint 에러 해결

--- api/src/main/kotlin/com/backgu/amaker/chat/controller/ChatRoomSwagger.kt
+++ api/src/main/kotlin/com/backgu/amaker/chat/controller/ChatRoomSwagger.kt
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 
