016d9d96ee3dde49d2408c5ba77a87071245cde6
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-07-26 11:36:55 +0900

    refactor: 컴포넌트 스캔범위 수정

--- api/src/main/kotlin/com/backgu/amaker/api/AMakerApiApplication.kt
+++ api/src/main/kotlin/com/backgu/amaker/api/AMakerApiApplication.kt
@@ -1,13 +1,15 @@
-package com.backgu.amaker
+package com.backgu.amaker.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableAsync
 
 @EnableAsync
-@SpringBootApplication
-@EntityScan(basePackages = ["com.backgu.amaker"])
+@EntityScan(basePackages = ["com.backgu.amaker.infra"])
+@EnableJpaRepositories(basePackages = ["com.backgu.amaker.infra"])
+@SpringBootApplication(scanBasePackages = ["com.backgu.amaker.api", "com.backgu.amaker.infra"])
 class AMakerApiApplication
 
 fun main(args: Array<String>) {
