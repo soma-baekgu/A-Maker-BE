067968e1e794fc2c09df3e168046129897cf72e9
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-07-28 23:24:40 +0900

    refactor: 단건 조회가 없기 때문에 Location url 삭제

--- api/src/main/kotlin/com/backgu/amaker/api/event/controller/EventCommentController.kt
+++ api/src/main/kotlin/com/backgu/amaker/api/event/controller/EventCommentController.kt
@@ -4,6 +4,7 @@ import com.backgu.amaker.api.event.dto.request.ReplyCommentCreateRequest
 import com.backgu.amaker.api.event.service.EventCommentFacadeService
 import com.backgu.amaker.api.security.JwtAuthentication
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PathVariable
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
 @RequestMapping("/api/v1")
@@ -23,19 +23,8 @@ class EventCommentController(
         @AuthenticationPrincipal token: JwtAuthentication,
         @PathVariable("event-id") eventId: Long,
         @RequestBody @Valid replyCommentCreateRequest: ReplyCommentCreateRequest,
-    ): ResponseEntity<Unit> =
-        ResponseEntity
-            .created(
-                ServletUriComponentsBuilder
-                    .fromCurrentRequest()
-                    .path("/{id}")
-                    .buildAndExpand(
-                        eventCommentFacadeService
-                            .createReplyComment(
-                                token.id,
-                                eventId,
-                                replyCommentCreateRequest.toDto(),
-                            ).id,
-                    ).toUri(),
-            ).build()
+    ): ResponseEntity<Unit> {
+        eventCommentFacadeService.createReplyComment(token.id, eventId, replyCommentCreateRequest.toDto())
+        return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
 }
