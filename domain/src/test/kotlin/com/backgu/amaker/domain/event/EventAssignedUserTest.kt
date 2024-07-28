41d05a433fca7259cb0d514c14e6f1cd8491cc87
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-07-28 20:15:57 +0900

    test: replyComment 생성 도메인 테스트 작성

---
+++ domain/src/test/kotlin/com/backgu/amaker/domain/event/EventAssignedUserTest.kt
@@ -0,0 +1,26 @@
+package com.backgu.amaker.domain.event
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import kotlin.test.Test
+
+@DisplayName("EventAssignedUser 테스트")
+class EventAssignedUserTest {
+    @Test
+    @DisplayName("isFinished 업데이트 테스트")
+    fun updateIsFinished() {
+        // given
+        val eventAssignedUser =
+            EventAssignedUser(
+                id = 1L,
+                eventId = 1L,
+                userId = "test-user-id",
+            )
+
+        // when
+        val updatedEventAssignedUser = eventAssignedUser.updateIsFinished(true)
+
+        // then
+        assertThat(updatedEventAssignedUser.isFinished).isTrue()
+    }
+}
