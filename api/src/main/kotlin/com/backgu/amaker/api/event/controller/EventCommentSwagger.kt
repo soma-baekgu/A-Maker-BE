a468579ab3f2cec8e46af850be70b47c5a4d79be
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-07-28 20:11:33 +0900

    docs: replyComment 생성 swagger 작성

---
+++ api/src/main/kotlin/com/backgu/amaker/api/event/controller/EventCommentSwagger.kt
@@ -0,0 +1,27 @@
+package com.backgu.amaker.api.event.controller
+
+import com.backgu.amaker.api.event.dto.request.ReplyCommentCreateRequest
+import com.backgu.amaker.api.security.JwtAuthentication
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
+
+@Tag(name = "eventComment", description = "이벤트 응답 API")
+interface EventCommentSwagger {
+    @Operation(summary = "reply 이벤트 응답 생성", description = "reply 이벤트 응답 생성합니다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "201",
+                description = "reply 이벤트 응답 생성 성공",
+            ),
+        ],
+    )
+    fun createReplyComment(
+        token: JwtAuthentication,
+        eventId: Long,
+        request: ReplyCommentCreateRequest,
+    ): ResponseEntity<Unit>
+}
