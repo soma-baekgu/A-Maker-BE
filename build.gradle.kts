ea799a782d5f22e0b5b58af66c310a36a1b825b2
Dltmd202 <dltmd202@gmail.com>
2024-07-28 16:26:56 +0900

    chore: notification 앱을 위한 build.gradle.kts 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,6 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.kapt")
 
     dependencies {
-        implementation("org.springframework.boot:spring-boot-starter-web")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
         implementation("org.springframework.boot:spring-boot-starter")
