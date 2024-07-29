1533b9a8f1cef0cef83be05b79bcc66167e6502c
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-07-29 11:00:16 +0900

    docs: 리플라이 응답 swagger 작성

--- api/src/main/kotlin/com/backgu/amaker/api/event/controller/EventCommentSwagger.kt
+++ api/src/main/kotlin/com/backgu/amaker/api/event/controller/EventCommentSwagger.kt
@@ -1,6 +1,11 @@
 package com.backgu.amaker.api.event.controller
 
+import com.backgu.amaker.api.common.dto.response.ApiResult
+import com.backgu.amaker.api.common.dto.response.PageResponse
+import com.backgu.amaker.api.event.dto.query.ReplyQueryRequest
 import com.backgu.amaker.api.event.dto.request.ReplyCommentCreateRequest
+import com.backgu.amaker.api.event.dto.response.ReplyCommentWithUserResponse
+import com.backgu.amaker.api.event.dto.response.ReplyCommentsViewResponse
 import com.backgu.amaker.api.security.JwtAuthentication
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
@@ -24,4 +29,19 @@ interface EventCommentSwagger {
         eventId: Long,
         replyCommentCreateRequest: ReplyCommentCreateRequest,
     ): ResponseEntity<Unit>
+
+    @Operation(summary = "reply 이벤트 응답 조회", description = "reply 이벤트 응답 조회합니다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "reply 이벤트 응답 조회 성공",
+            ),
+        ],
+    )
+    fun findReplyComments(
+        token: JwtAuthentication,
+        eventId: Long,
+        replyQueryRequest: ReplyQueryRequest,
+    ): ResponseEntity<ApiResult<PageResponse<ReplyCommentWithUserResponse>>>
 }
