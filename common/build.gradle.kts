d8751add7d60cdaf92aa33e07650e3140ca30524
Dltmd202 <dltmd202@gmail.com>
2024-08-03 17:53:39 +0900

    chore: common 모듈 생성

---
+++ common/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.backgu.amaker"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.auth0:java-jwt:3.18.3")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.getByName<Jar>("bootJar") {
+    enabled = false
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
