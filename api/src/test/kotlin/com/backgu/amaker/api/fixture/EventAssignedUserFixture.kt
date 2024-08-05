70eae959eb24aa47e6bb731422a7560351e36357
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-08-05 19:16:29 +0900

    refactor: 코드 컨벤션 변경

--- api/src/test/kotlin/com/backgu/amaker/api/fixture/EventAssignedUserFixture.kt
+++ api/src/test/kotlin/com/backgu/amaker/api/fixture/EventAssignedUserFixture.kt
@@ -1,17 +1,17 @@
 package com.backgu.amaker.api.fixture
 
 import com.backgu.amaker.infra.jpa.event.entity.EventAssignedUserEntity
-import com.backgu.amaker.infra.jpa.event.repository.EventAssignedRepository
+import com.backgu.amaker.infra.jpa.event.repository.EventAssignedUserRepository
 import org.springframework.stereotype.Component
 
 @Component
 class EventAssignedUserFixture(
-    val eventAssignedRepository: EventAssignedRepository,
+    val eventAssignedUserRepository: EventAssignedUserRepository,
 ) {
     fun createPersistedEventAssignedUser(
         userId: String = "test-user-id",
         eventId: Long = 1L,
-    ) = eventAssignedRepository.save(
+    ) = eventAssignedUserRepository.save(
         EventAssignedUserEntity(
             userId = userId,
             eventId = eventId,
--- infra/src/main/kotlin/com/backgu/amaker/infra/jpa/event/repository/EventAssignedUserRepository.kt
+++ infra/src/main/kotlin/com/backgu/amaker/infra/jpa/event/repository/EventAssignedUserRepository.kt
@@ -3,7 +3,7 @@ package com.backgu.amaker.infra.jpa.event.repository
 import com.backgu.amaker.infra.jpa.event.entity.EventAssignedUserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface EventAssignedRepository : JpaRepository<EventAssignedUserEntity, Long> {
+interface EventAssignedUserRepository : JpaRepository<EventAssignedUserEntity, Long> {
     fun findAllByEventId(eventId: Long): List<EventAssignedUserEntity>
 
     fun findByEventIdIn(eventIds: List<Long>): List<EventAssignedUserEntity>
