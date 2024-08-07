410778f4fedc3ece5f5c7075514707cee90d7f0e
Dltmd202 <dltmd202@gmail.com>
2024-08-07 14:04:04 +0900

    fix: realtime 서버를 직접 등록하도록 수정

--- api/src/main/kotlin/com/backgu/amaker/api/realtime/dto/response/RealTimeServerHealthResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.backgu.amaker.api.realtime.dto.response
-
-data class RealTimeServerHealthResponse(
-    val status: String,
-) {
-    fun isValid(): Boolean = status == "UP"
-}
--- realtime/src/main/kotlin/com/backgu/amaker/realtime/orchestration/dto/ServerInformation.kt
+++
@@ -1,8 +0,0 @@
-package com.backgu.amaker.realtime.orchestration.dto
-
-data class ServerInformation(
-    val id: Long,
-    val schema: String,
-    val address: String,
-    val port: Int,
-)
