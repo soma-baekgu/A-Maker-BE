e40d6b308b1a33a4575b8070f584af73a3e847a7
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-08-12 11:13:53 +0900

    feat: 워크스페이스 유저 조회에도 사용하게 변경

--- api/src/main/kotlin/com/backgu/amaker/api/workspace/dto/response/WorkspaceUserResponse.kt
+++ api/src/main/kotlin/com/backgu/amaker/api/workspace/dto/response/WorkspaceUserResponse.kt
@@ -6,8 +6,12 @@ import com.backgu.amaker.domain.workspace.WorkspaceUserStatus
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class WorkspaceUserResponse(
+    @Schema(description = "이름", example = "홍길동")
+    val name: String,
     @Schema(description = "이메일", example = "abc@example.com")
     val email: String,
+    @Schema(description = "프로필 사진", example = "https://example.com/abc.jpg")
+    val picture: String,
     @Schema(description = "워크스페이스 id", example = "1")
     val workspaceId: Long,
     @Schema(description = "워크스페이스 권한(MEMBER, LEADER)", example = "MEMBER")
@@ -18,7 +22,9 @@ data class WorkspaceUserResponse(
     companion object {
         fun of(workspaceUser: WorkspaceUserDto) =
             WorkspaceUserResponse(
+                name = workspaceUser.name,
                 email = workspaceUser.email,
+                picture = workspaceUser.picture,
                 workspaceId = workspaceUser.workspaceId,
                 workspaceRole = workspaceUser.workspaceRole,
                 status = workspaceUser.status,
