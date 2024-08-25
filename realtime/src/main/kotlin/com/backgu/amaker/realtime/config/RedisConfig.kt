026603a9bd558c59a4ca348b40a91bd4094a2ce2
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-08-25 23:25:18 +0900

    chore: cluster 설정 추가

--- realtime/src/main/kotlin/com/backgu/amaker/realtime/config/RedisConfig.kt
+++ realtime/src/main/kotlin/com/backgu/amaker/realtime/config/RedisConfig.kt
@@ -4,6 +4,7 @@ import com.backgu.amaker.infra.redis.session.SessionRedisData
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
@@ -11,6 +12,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
+@Profile("!prod")
 @ConfigurationProperties(prefix = "spring.data.redis")
 class RedisConfig {
     lateinit var host: String
