02e6bce87062db9fa9cb37ba4303bf46ae0c5796
Dltmd202 <dltmd202@gmail.com>
2024-09-02 14:39:05 +0900

    fix: 초대받은 사용자가 워크스페이스 정보를 조회할 수 있도록 수정

--- infra/src/main/kotlin/com/backgu/amaker/infra/jpa/workspace/repository/WorkspaceUserRepository.kt
+++ infra/src/main/kotlin/com/backgu/amaker/infra/jpa/workspace/repository/WorkspaceUserRepository.kt
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 interface WorkspaceUserRepository : JpaRepository<WorkspaceUserEntity, Long> {
-    @Query("select wu.workspaceId from WorkspaceUser wu where wu.userId = :userId and wu.status = 'ACTIVE'")
+    @Query("select wu.workspaceId from WorkspaceUser wu where wu.userId = :userId")
     fun findWorkspaceIdsByUserId(
         @Param("userId") userId: String,
     ): List<Long>
